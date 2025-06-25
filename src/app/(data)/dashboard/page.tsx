2573ca4e7a343132cc8eb6b40e764ea97588eefe
parag koche <you@example.com>
2025-06-25 15:07:59 +0530

    fix: refactor Page component to use state for data fetching and improve rendering

--- src/app/(data)/dashboard/page.tsx
+++ src/app/(data)/dashboard/page.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-import Link from "next/link";
-import EditButton from "./_components/editButton";
-import DeleteButton from "./_components/deleteButton";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import RecordTable from "./_record";
 import { getAllOcr } from "@/action/ocr";
-const Page = async () => {
-  const ocrData = await getAllOcr();
+import { ocr } from "@/generated/prisma";
+
+const Page = () => {
+  const [ocrData, setOcrData] = useState<ocr[]>([]);
+
+  const fetchData = async () => {
+    const data = await getAllOcr();
+    setOcrData(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-      <h1 className="text-3xl font-semibold my-3"> Records</h1>
+      <h1 className="text-3xl font-semibold my-3">Records</h1>
       <RecordTable data={ocrData} />
     </>
   );
